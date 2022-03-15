b434714da2bf75dc42a1f18fdc6fb5043171b5c6
ajosgonsot <a_josgonsot@iestriana.com>
2022-03-15 14:39:35 +0100

    Ejercicios

---
+++ Prueba/Ejercicio Clase/Ejercicios HTML/JavaTask/Task10/js/funciones.js
@@ -0,0 +1,8 @@
+function borde1(){
+    var Tabla = document.getElementById("Tabla")
+    Poner.style.borderColor = '#000000' ;
+}
+function borde2(){
+    var Tabla = document.getElementById("Tabla")
+    Quitar.style.borderColor = '#FFFFFF';
+}
---
+++ Prueba/Ejercicio Clase/Ejercicios HTML/JavaTask/Task11/js/funciones.js
@@ -0,0 +1,15 @@
+var Añadir = document.getElementById("Añadir")
+var Fin = document.getElementById("Fin")
+var Numero = document.getElementById("Numero")
+
+function Span1(){
+    if (Numero >= '0'){
+        document.write("El mayor numero es:" + numero + " (Numero)");
+    }
+    else if (Numero < Numero){
+        document.write("El Menor numero es:" + numero + " (Numero)");
+    }
+    if (Numero + Numero){
+        document.write("La suma de los positivos es:" + numero+numero + " (Numero)");
+    }
+}
